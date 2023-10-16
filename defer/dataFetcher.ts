03882be034f5378a93bcc7a2629fb7c742bed8c4
unknown <ahmedmidan8@gmail.com>
2023-10-16 20:22:43 +0530

    fix: defer wrapper added

--- defer/dataFetcher.ts
+++ defer/dataFetcher.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { defer } from "@defer/client";
 import axios from "axios";
 import { Resend } from "resend";
-import { DailyQuote } from "@/emails/DailyQuote";
+import DailyQuote from "@/emails/DailyQuote";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -16,9 +16,10 @@ const dataFetcher = async () => {
       },
     });
 
-    console.log(users, data);
+    console.log(data);
 
     users.forEach(async (user) => {
+      console.log(user);
       await resend.sendEmail({
         from: "quotidian@email.com",
         to: user.email,
