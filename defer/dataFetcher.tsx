f73580780ac68a2a683bfa2c964764a452758c57
unknown <ahmedmidan8@gmail.com>
2023-10-22 18:52:52 +0530

    fix: added await to post request

--- defer/dataFetcher.tsx
+++ defer/dataFetcher.tsx
@@ -2,26 +2,23 @@ import { db } from "@/db";
 import { defer } from "@defer/client";
 import axios from "axios";
 import DailyQuote from "@/emails/DailyQuote";
-import { currentUTCHour } from "@/lib/dayjs";
+import { currentUTCHour, currentUTCTimestamp } from "@/lib/dayjs";
 import { render } from "@react-email/components";
 
 const dataFetcher = async () => {
   try {
     const { data } = await axios.get("https://api.quotable.io/quotes/random");
-    let currHour = currentUTCHour();
 
     const users = await db.user.findMany({
       where: {
         isSub: true,
         prefHour: {
-          gte: currHour,
-          lt: (currHour + 1) % 24,
+          equals: currentUTCHour(),
         },
       },
     });
 
     users.forEach(async (user) => {
-      console.log(user.email, currHour);
       const emaildata = {
         from: "Quotidian <onboarding@resend.dev>",
         to: user.email,
@@ -37,9 +34,10 @@ const dataFetcher = async () => {
           }
         ),
       };
-      axios.post("https://api.resend.com/email", emaildata, {
+      const res = await axios.post("https://api.resend.com/email", emaildata, {
         headers: { Authorization: `Bearer ${process.env.RESEND_API_KEY}` },
       });
+      console.log(user.email, currentUTCTimestamp(), res.data.id);
     });
   } catch (err) {
     console.log(err);
