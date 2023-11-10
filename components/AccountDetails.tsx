08f772d73a6701d1d9c9ab4854b7559fe132c9d1
unknown <ahmedmidan8@gmail.com>
2023-11-10 18:53:41 +0530

    fix: fixed pref-hour

--- components/AccountDetails.tsx
+++ components/AccountDetails.tsx
@@ -3,6 +3,8 @@ import { User2 } from "lucide-react";
 import { CardContent } from "@/components/ui/card";
 import { fetchAccountInfoById } from "@/lib/data";
 import { FC } from "react";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Button, buttonVariants } from "./ui/button";
 
 interface UserId {
   id: string;
@@ -11,7 +13,7 @@ interface UserId {
 const AccountDetails: FC<UserId> = async ({ id }: UserId) => {
   const user = await fetchAccountInfoById(id);
   return (
-    <CardContent className="space-y-2">
+    <CardContent className="md:flex md:items-center md:justify-between">
       <div className="space-y-4 md:flex md:items-center md:space-x-4 md:space-y-0">
         <Avatar>
           <AvatarImage src={user?.displayPicture || ""} />
@@ -26,6 +28,9 @@ const AccountDetails: FC<UserId> = async ({ id }: UserId) => {
           <p className="text-sm text-muted-foreground">{user?.email}</p>
         </div>
       </div>
+      <Button variant="outline" className="mt-4 md:mt-0">
+        <LogoutLink>Log out</LogoutLink>
+      </Button>
     </CardContent>
   );
 };
--- lib/dayjs.ts
+++ lib/dayjs.ts
@@ -2,15 +2,15 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 dayjs.extend(utc);
 
-export function currentUTCTimestamp() {
-  return dayjs().utc();
+export function convertToUTCTimestamp(timestamp: Date) {
+  return dayjs(timestamp).utc().format();
 }
 
 export function currentUTCHour() {
   return dayjs().utc().hour();
 }
 
-export function initialTimeSetter(t = 8) {
+export function timeSetter(t = 8) {
   let initialTime = dayjs().local().set("hour", t).set("minute", 0).utc();
   return { timestamp: initialTime.format(), hour: initialTime.hour() };
 }
